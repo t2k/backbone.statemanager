888c40c57f212a7cd9b809a8ac2625e676bd0104
Ted T. Killilea <lrrs259@gmail.com>
2014-09-06 16:11:00 -0400

    Create gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,5 @@
+// leave this file at project root to allow gulp to run gulpfile.coffee natively
+// register coffe for cli (the new way in coffee #1.7.1)
+// heroku reguires the full gulpfile.js
+require('coffee-script/register');
+require('./gulpfile.coffee');
